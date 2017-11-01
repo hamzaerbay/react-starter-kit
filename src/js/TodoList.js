417c38b1109bb2cbe4cf8bb2c557c6710b5e97c1
Hamza Erbay <hamza@udemy.com>
2017-11-01 01:36:12 -0700

    Mobx Todo list

--- src/js/TodoList.js
+++ src/js/TodoList.js
@@ -3,7 +3,67 @@ import { observer } from 'mobx-react';
 
 @observer
 export default class TodoList extends Component {
+	constructor(props) {
+		super(props);
+		this.filter = this.filter.bind(this);
+	}
+	filter(e) {
+		this.props.store.filter = e.target.value;
+	}
+	createNew(e) {
+		e.preventDefault();
+		const newItem = this.newItem.value;
+		console.log(newItem);
+		this.props.store.createTodo(newItem);
+
+		this.addForm.reset();
+	}
+	toggleComplete(todo) {
+		todo.complete = !todo.complete;
+	}
 	render() {
-		return <h1>{this.props.store.todos[1]}</h1>;
+		const { clearComplete, filter, filteredTodos, todos } = this.props.store;
+		const todoItems = filteredTodos.map(todo => (
+			<li className="todo-list__item" key={todo.id}>
+				<label htmlFor={todo.id}>
+					<input
+						id={todo.id}
+						type="checkbox"
+						onChange={this.toggleComplete.bind(this, todo)}
+						value={todo.complete}
+						checked={todo.complete}
+					/>
+					{todo.value}
+				</label>
+			</li>
+		));
+		return (
+			<div>
+				<h1>
+					<i className="fa fa-shopping-basket" aria-hidden="true" />
+					Shopping List
+				</h1>
+				<div>{filter}</div>
+				<form
+					className="add-item__form"
+					ref={input => {
+						this.addForm = input;
+					}}
+					onSubmit={e => {
+						this.createNew(e);
+					}}
+				>
+					<input className="add-item__input" ref={input => (this.newItem = input)} />
+					<button type="submit" className="btn btn--primary">
+						Add
+					</button>
+				</form>
+				<input className="filter" value={filter} onChange={this.filter} />
+				<ul className="todo-list">{todoItems}</ul>
+				<a href="#" onClick={clearComplete}>
+					Clear
+				</a>
+			</div>
+		);
 	}
 }
--- src/js/TodoStore.js
+++ src/js/TodoStore.js
@@ -1,13 +1,31 @@
-import { autorun, observable } from 'mobx';
+import { autorun, computed, observable } from 'mobx';
 
-class TodoStore {
-	@observable todos = ['Learn React', 'Learn Mobx'];
-	@observable filter = '';
+class Todo {
+	@observable value;
+	@observable id;
+	@observable complete;
+	constructor(value) {
+		this.value = value;
+		this.id = Date.now();
+		this.complete = false;
+	}
 }
-var store = (window.store = new TodoStore());
+class TodoStore {
+	@observable
+	todos = [{ id: 1, value: 'react', complete: true }, { id: 2, value: 'mobx', complete: false }];
+	@observable filter = '';
+	@computed
+	get filteredTodos() {
+		let matchesFilter = new RegExp(this.filter, '');
+		return this.todos.filter(todo => !this.filter || matchesFilter.test(todo.value));
+	}
+	createTodo(value) {
+		this.todos.push(new Todo(value));
+	}
+	clearComplete = () => {
+		const incompleteTodos = this.todos.filter(todo => !todo.complete);
+		this.todos.replace(incompleteTodos);
+	};
+}
 
-export default store;
-autorun(() => {
-	console.log(`filter ${store.filter}`);
-	console.log(store.todos[0]);
-});
+export default new TodoStore();
