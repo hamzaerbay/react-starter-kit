8b15405ce695cd8d3fdedd8c4d4a5e970c56fb93
Hamza Erbay <hamza@udemy.com>
2017-11-12 16:55:37 -0800

    webpack formatted

--- webpack.config.js
+++ webpack.config.js
@@ -1,40 +1,39 @@
 const ExtractTextPlugin = require('extract-text-webpack-plugin');
 const path = require('path');
+
 module.exports = {
   entry: './src/js/app.js',
   module: {
-      rules: [
-        {
-          test: /\.scss$/,
-          use: ExtractTextPlugin.extract({
-            fallback: 'style-loader',
-            use: [ 'css-loader', 'sass-loader']
-          }
-        )
+    rules: [
+      {
+        test: /\.scss$/,
+        use: ExtractTextPlugin.extract({
+          fallback: 'style-loader',
+          use: ['css-loader', 'sass-loader'],
+        }),
       },
       {
         test: /\.js$/,
         exclude: /node_modules/,
-        loader: 'babel-loader'
-      }
-    ]
+        loader: 'babel-loader',
+      },
+    ],
   },
   devServer: {
     contentBase: path.join(__dirname),
     compress: true,
     // port: 9000,
-    stats:'errors-only',
+    stats: 'errors-only',
     open: true,
   },
   plugins: [
     new ExtractTextPlugin({
       allChunks: true,
-      filename: './dist/styles.css'
-    })
+      filename: './dist/styles.css',
+    }),
   ],
   output: {
-
     path: path.resolve(__dirname),
-    filename: './dist/app.bundle.js'
-  }
-}
+    filename: './dist/app.bundle.js',
+  },
+};
