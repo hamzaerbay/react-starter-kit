a4b1c013670b8c759e353fb59faf4853bdd5082c
Hamza Erbay <hamzaerbay@gmail.com>
2023-09-15 18:02:53 -0700

    refactor(app): TSX migration

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,9 @@
 import React from 'react';
-import { createRoot } from 'react-dom';
+import { createRoot } from 'react-dom/client';
 
 const App = () => {
   return <div>HOLA!!!!</div>;
 };
 
-const root = createRoot(document.getElementById('root'));
+const root = createRoot(document.getElementById('root')!);
 root.render(React.createElement(App));
