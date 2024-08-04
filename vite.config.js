df5bdce02ce9a50c5294eaffd880817ccd88e00b
Hamza Erbay <hamzaerbay@gmail.com>
2024-08-04 00:28:24 -0700

    fix(app): add import aliases for root and components directory

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,19 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import path from 'path';
 
 export default defineConfig({
-  plugins: [react()],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+      '@components': path.resolve(__dirname, './src/components'),
+    },
+    plugins: [react()],
+  },
   // root: 'src',
   test: {
     globals: true,
-    environment: "jsdom",
+    environment: 'jsdom',
     setupFiles: './src/__tests__/setup.ts',
   },
 });
